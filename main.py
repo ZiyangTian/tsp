e1cface15e537d8ec674fb47921c1e1a34db37b4
ZiyangTian <tianziyang@buaa.edu.cn>
2020-08-10 21:01:05 +0800

    v2

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import rkga.rkga as rkga
 
 
 def main():
+    regions = tspn.EllipsoidRegion.from_randomly_generated(
+        (20,), [0.]*6, [1., 1., 1., 0.1, 0.1, 0.1]*6, 3)
     solver = rkga.RandomKeyGeneticAlgorithm()
-    regions = tspn.EllipsoidRegion.from_randomly_generated((10,), [0.]*6, [1.]*6, 3)
-    solver.compile(regions)
-    solver.run()
+    solver.compile(regions, use_cuda=False)
+    solver.run(10000)
 
 
 if __name__ == '__main__':
--- rkga/rkga.py
+++ rkga/rkga.py
@@ -11,18 +11,13 @@ class RKGAConfig(object):
     len_vector: V
     """
     selection_proportion = 0.35
-
     crossover_proportion = 0.55
     crossover_threshold = 0.3
     crossover_laplace_lambda = 0.
     crossover_laplace_mu = 1.
-
     mutation_prop = 0.01
 
-    population_size = 1000
-
-    region_type = 'EllipsoidRegion'
-    max_num_generations = 10000
+    population_size = 10000
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -36,7 +31,6 @@ class RKGAConfig(object):
 class RandomKeyGeneticAlgorithm(RKGAConfig):
     def __init__(self, **kwargs):
         super(RandomKeyGeneticAlgorithm, self).__init__(**kwargs)
-
         self._region_type = None
         self._region_data = None
         self._len_params = None
@@ -53,6 +47,8 @@ class RandomKeyGeneticAlgorithm(RKGAConfig):
         self._best_fitness_data = None
         self._num_generations = 0
 
+        self._use_cuda = False
+
         self.__compiled = False
         self.__selected_fractional_temp_data = None
         self.__selected_vector_temp_data = None
@@ -61,9 +57,11 @@ class RandomKeyGeneticAlgorithm(RKGAConfig):
         self.__migration_fractional_temp_data = None
         self.__migration_vector_temp_data = None
 
-    def compile(self, regions: tspn.Region):
+    def compile(self, regions: tspn.Region, use_cuda=False):
+        self._use_cuda = use_cuda
+
         self._region_type = type(regions)
-        self._region_data = torch.tensor(regions.parameters)  # RP
+        self._region_data = self.maybe_cuda_tensor(torch.tensor(regions.parameters))  # RP
         self._len_params = regions.parameters.shape[-1]
         self._num_regions = regions.shape[0]
         self._len_vector = regions.len_vector
@@ -71,13 +69,13 @@ class RandomKeyGeneticAlgorithm(RKGAConfig):
         self.__compiled = True
         self.initialize()
 
-    def run(self, max_descending_generations=None):
+    def run(self, max_num_generations, max_descending_generations=None):
         self._compiled()
         descending = 0
         last_fitness = 0
-        for g in range(self.max_num_generations):
+        for g in range(max_num_generations):
             self.evolute()
-            best_fitness = self._best_fitness_data.numpy()
+            best_fitness = self.maybe_numpy_tensor(self._best_fitness_data)
             print(best_fitness)
             if best_fitness > last_fitness:
                 descending += 1
@@ -115,20 +113,20 @@ class RandomKeyGeneticAlgorithm(RKGAConfig):
     def _crossover(self):
         indices_1 = torch.randint(low=0, high=self.selection_size, size=(self.crossover_size,))
         indices_2 = torch.randint(low=0, high=self.selection_size, size=(self.crossover_size,))
-        random_values = torch.rand(self.crossover_size, self._num_regions)
+        random_values = self.maybe_cuda_tensor(torch.rand(self.crossover_size, self._num_regions))
         self.__crossover_fractional_temp_data = torch.where(
             random_values > self.crossover_threshold,
             self.__selected_fractional_temp_data[indices_1],
             self.__selected_fractional_temp_data[indices_2])
 
-        laplace_values = self.crossover_laplace.sample((self.crossover_size,))
+        laplace_values = self.maybe_cuda_tensor(self.crossover_laplace.sample((self.crossover_size,)))
         civ_1 = self.__selected_vector_temp_data[indices_1]
         civ_2 = self.__selected_vector_temp_data[indices_2]
         self.__crossover_vector_temp_data = civ_1 + laplace_values[:, None, None] * (civ_1 - civ_2)
 
     def _mutate(self):
-        selected_mutation_random = torch.rand(self.selection_size, self._num_regions)
-        crossover_mutation_random = torch.rand(self.crossover_size, self._num_regions)
+        selected_mutation_random = self.maybe_cuda_tensor(torch.rand(self.selection_size, self._num_regions))
+        crossover_mutation_random = self.maybe_cuda_tensor(torch.rand(self.crossover_size, self._num_regions))
 
         fractional, vector = self._initialize_individuals_randomly(self.selection_size)
         self.__selected_fractional_temp_data = torch.where(
@@ -169,9 +167,22 @@ class RandomKeyGeneticAlgorithm(RKGAConfig):
         self._best_vector_data = self._vector_data[self._best_index_data]
         self._best_fitness_data = self._fitness_data[self._best_index_data]
 
+    def maybe_cuda_tensor(self, tensor):
+        if not (self._use_cuda and torch.cuda.is_available()):
+            return tensor
+        return tensor.cuda()
+
+    @staticmethod
+    def maybe_numpy_tensor(tensor):
+        if tensor.is_cuda:
+            return tensor.cpu().numpy()
+        return tensor.numpy()
+
     def _initialize_individuals_randomly(self, n):
         fractional = torch.rand(n, self._num_regions)  # nR
         vector = torch.randn(n, self._num_regions, self._len_vector).softmax(dim=-1)  # nRV
+        fractional = self.maybe_cuda_tensor(fractional)
+        vector = self.maybe_cuda_tensor(vector)
         return fractional, vector
 
     def _compute_fitness(self, fractional, vector):
