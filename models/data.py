917abe90d7f02b823cdc621b839685ef18ce2eb0
ZiyangTian <tianziyang@buaa.edu.cn>
2020-08-19 17:54:20 +0800

    model

--- models/data.py
+++ models/data.py
@@ -14,7 +14,7 @@ from torch.utils import data as torch_data
 
 
 class TSPDataset(torch_data.Dataset):
-    def __init__(self, pattern, max_num_nodes=None, shift_rank_randomly=True):
+    def __init__(self, pattern, shift_rank_randomly=True):
         data_files = glob.glob(pattern)
         param_files = list(filter(lambda f: f.endswith('.parameters'), data_files))
         param_prefix = list(map(lambda f: f[:-11], param_files))
@@ -27,33 +27,42 @@ class TSPDataset(torch_data.Dataset):
         self.param_files = list(map(lambda p: p + '.parameters', sorted_prefix))
         self.rank_files = list(map(lambda p: p + '.rank', sorted_prefix))
 
-        self.max_num_nodes = max_num_nodes
         self.shift_rank_randomly = shift_rank_randomly
 
     def __getitem__(self, item):
         parameters = torch.tensor(np.array(pd.read_csv(self.param_files[item], header=None)), dtype=torch.float)
         rank = np.array(pd.read_csv(self.rank_files[item], header=None))[0].tolist()
-        rank_length = len(rank)
+        length = len(rank)
         if self.shift_rank_randomly:
-            random_int = random.randint(0, rank_length)
+            random_int = random.randint(0, length)
             rank = rank[random_int:] + rank[: random_int]
         rank = torch.tensor(rank, dtype=torch.int)
-
-        if self.max_num_nodes is None:
-            return parameters, rank
-        mask = torch.where(torch.arange(self.max_num_nodes) < rank_length, torch.tensor(0), torch.tensor(1))
-        parameters = torch.nn.functional.pad(parameters, [0, 0, 0, self.max_num_nodes - parameters.shape[-2]])
-        rank = torch.nn.functional.pad(rank, [0, self.max_num_nodes - rank.shape[-1]])
-        return parameters, rank, mask
+        return parameters, rank, torch.tensor(length)
 
     def __len__(self):
         return self.length
 
 
+class TSPDataLoader(torch_data.DataLoader):
+    def __init__(self, pattern, shift_rank_randomly=True, batch_size=1, shuffle=False, **kwargs):
+        dataset = TSPDataset(pattern, shift_rank_randomly=shift_rank_randomly)
+
+        def collate_fn(examples):
+            parameters, ranks, lengths = zip(*examples)
+            padded_parameters = torch.nn.utils.rnn.pad_sequence(parameters)
+            padded_ranks = torch.nn.utils.rnn.pad_sequence(ranks)
+            lengths = torch.stack(lengths)
+            return padded_parameters, padded_ranks, lengths
+
+        super(TSPDataLoader, self).__init__(
+            dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn, **kwargs)
+
+
 def main():
-    pattern = r'E:\Programs\DataSets\tsp\*'
-    dataset = TSPDataset(pattern, max_num_nodes=50)
-    print(dataset[0][0].shape, dataset[0][1].shape)
+    pattern = r'/Users/Tianziyang/Desktop/data/tsp/*'
+    dl = TSPDataLoader(pattern, batch_size=2)
+    for x in dl:
+        print(x)
 
 
 if __name__ == '__main__':
--- models/networks.py
+++ models/networks.py
@@ -6,40 +6,144 @@ from models import data
 
 
 class Encoder(torch.nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size, num_layers, dropout=0.):
         super(Encoder, self).__init__()
         self.hidden_size = hidden_size
 
         self.linear = torch.nn.Linear(input_size, hidden_size)
-        self.gru = torch.nn.GRU(input_size, hidden_size, num_layers)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.gru = torch.nn.GRU(hidden_size, hidden_size, num_layers=num_layers)
 
-    def forward(self, inputs, hidden, mask):
-        outputs = self.linear(inputs.permute(1, 0, 2))
-        outputs, hidden = self.gru(outputs)
-        parameters = parameters
+    def forward(self, inputs, lengths):
+        """
+
+        :param inputs: max_len, batch_size, 2
+        :param lengths: batch_size
+        :return:
+        """
+        rnn_inputs = self.linear(inputs.permute(1, 0, 2))
+        rnn_inputs = self.dropout(rnn_inputs)
+        packed_rnn_inputs = torch.nn.utils.rnn.pack_padded_sequence(rnn_inputs, lengths)
+        packed_rnn_outputs, hidden = self.gru(packed_rnn_inputs)
+        padded_rnn_outputs = torch.nn.utils.rnn.pad_packed_sequence(packed_rnn_outputs)
+        return padded_rnn_outputs, hidden, lengths
 
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
+class BahdanauAttention(torch.nn.Module):
+    def __init__(self, hidden_size):
+        super(BahdanauAttention, self).__init__()
+        self.w1 = torch.nn.Linear(hidden_size, hidden_size)
+        self.w2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.v = torch.nn.Linear(hidden_size, 1)
+
+    def call(self, query, values):
+        # query hidden state shape == (1, batch_size, hidden size)
+        # values shape == (max_len, batch_size, hidden size)
+        query = query.permute(1, 0, 2)
+        values = values.permute(1, 0, 2)
+
+        # score shape == (batch_size, max_length, 1)
+        score = self.v(torch.nn.functional.tanh(self.w1(query) + self.w2(values)))
+
+        # attention_weights shape == (batch_size, max_len, 1)
+        attention_weights = torch.nn.functional.softmax(score, dim=1)
+
+        context_vector = attention_weights * values
+        context_vector = context_vector.sum(dim=1)  # (batch_size, hidden_size)
+
+        return attention_weights, context_vector
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, drouput):
+        super(Decoder, self).__init__()
         self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.linear = torch.nn.Linear(input_size, hidden_size)
+        self.dropout = torch.nn.Dropout(drouput)
+        self.gru = torch.nn.GRU(hidden_size, hidden_size, num_layers=num_layers)
+        self.attention = BahdanauAttention(hidden_size)
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
+    def call(self, hidden, encoder_outputs, inputs):
+        """
+        :param inputs: (batch_size, input_size) or (max_len, batch_size, input_size)
+        :param hidden: num_layers, batch_size, hidden_size
+        :param encoder_outputs: max_len, batch_size, hidden_size
+        :return:
+        """
+        hidden = hidden.mean(dim=0, keep_dim=True)
+        attention_weights, context_vector = self.attention(hidden, encoder_outputs)
+        output = attention_weights
+
+        if inputs.ndim == 3:
+            inputs = torch.gather(inputs, 0, output)  # !!!
+        inputs = inputs[None, ...]
+        rnn_inputs = self.linear(inputs)
+        rnn_inputs = self.dropout(rnn_inputs)
+        _, hidden = self.gru(torch.cat([rnn_inputs, context_vector[None, ...]], dim=-1), hidden)
         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class PointerNetwork(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, encoder_rnn_layers, decoder_rnn_layers,
+                 encoder_dropout=0, decoder_dropout=0):
+        super(PointerNetwork, self).__init__()
+        self.encoder = Encoder(input_size, hidden_size, encoder_rnn_layers, encoder_dropout)
+        self.decoder = Decoder(input_size, hidden_size, decoder_rnn_layers, decoder_dropout)
+
+    def forward(self, inputs, lengths, targets=None):
+        """
+
+        :param inputs: max_len, batch_size, 2
+        :param lengths: batch_size
+        :param targets: batch_size, max_len
+        :return:
+        """
+        max_len, batch_size, input_size = inputs.shape
+        encoder_outputs, hidden = self.encoder(inputs, lengths)
+
+        if self.training:  # teacher forcing
+            decoder_inputs = torch.gather(inputs, 1, targets.permute(1, 0))
+        else:
+            decoder_inputs = inputs
+
+        decoder_outputs = []
+        for i in range(max_len):
+            if self.training:
+                output, hidden = self.decoder(hidden, encoder_outputs, inputs=decoder_inputs[i])
+            else:
+                output, hidden = self.decoder(hidden, encoder_outputs, inputs=decoder_inputs)
+            decoder_outputs.append(output)
+        outputs = torch.cat(decoder_outputs, dim=-1)
+        return outputs.permute(0, 2, 1)  # batch_size, source_max_len, target_max_len
+
+
+def loss_fn(inputs, targets, length=None):
+    """
+
+    :param inputs: batch_size, source_max_len, target_max_len
+    :param targets: batch_size, max_len
+    :param length: batch_size
+    :return:
+    """
+    max_len = targets.shape[-1]
+    mask = torch.where(torch.arange(max_len) < length, torch.tensor(1.), torch.tensor(0.))
+    loss = torch.nn.functional.cross_entropy(inputs, targets, reduction='none') * mask
+    loss = torch.mean(loss)
+    return loss
 
 
 def main():
     pattern = r'E:\Programs\DataSets\tsp\*'
-    dataset = data.TSPDataset(pattern, max_num_nodes=50, shift_rank_randomly=True)
+    dataset = data.TSPDataset(pattern, shift_rank_randomly=True)
     data_loader = torch_data.DataLoader(dataset, batch_size=4, shuffle=True)
     for parameters, rank, mask in data_loader:
         break
+
+    a = torch.tensor([[1.], [2.], [3.]])
+    b = torch.tensor([[1.]])
+    c = torch.tensor([[1.], [2.]])
+    d = torch.nn.utils.rnn.pad_sequence([a, b, c])
+    e = torch.nn.utils.rnn.pack_padded_sequence(d, [3, 1, 2], enforce_sorted=False)
+    layer = torch.nn.GRU(1, 3, 2)
